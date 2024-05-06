6956795633293d5349b9885726dc573fc98d4196
sarahwierzbicki <168492907+sarahwierzbicki@users.noreply.github.com>
2024-05-06 14:01:29 -0400

    Update RenameProduct.java

--- RenameProduct.java
+++ RenameProduct.java
@@ -6,22 +6,44 @@
 //WHERE prodid = 'p1'
 
 // Load the PostgreSQL driver
+package renameProduct;
 public class RenameProduct {
   public static void main(String args[] throws SQLException, IOException, ClassNotFoundException {
 
     // load PostgreSQL driver
-    Connection conn = DriverManager.getConnection
+    Connection conn = DriverManager.getConnection("jdbc:postgresql
 
     // ensure atomicity
       conn.setAutoCommit(false);
     // ensure isolation
       conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+
+    // prepared statement
+    public void updateProduct(
+    String updateProduct = "update PRODUCT " + "set PRODID = ? where PRODID = ?";
+    String updateStock = "update STOCK " + "set PRODID = ? where PRODID = ?";
+      PreparedStatement updateP1 = con.prepareStatement(updateProduct);
+      PreapredStatement updateP2 = con.prepareStatement(updateStock);
+    // change PRODID column of p1 to pp1 for updateP1
+    updateP1.setString(1 , "pp1");
+    updateP1.setString(1, "p1");
+    updateP1.executeUpdate();
+
+    // change PRODID column in Stock(updatep2)
+    updateP2.setString(1, "pp1");
+    updateP2.setString(1, "p1");
+    updateP2.executeUpdate();
+    
+
+    // create result set and statement objects
+    ResultSet rs = null;
     Statement stmt1 = null;
     try {
         // create statement 
         stmt1 = conn.createStatement();
         // execute update to change name of p1 to pp1
-        stmt1.executeUpdate("update prodid set name = 'pp1' where prodid = 'p1"
+        stmt1.executeUpdate("update prodid set name = 'pp1' where prodid = 'p1")
+        stmt1.executeUpdate("update prodid set name = 'pp1' where prodid = 'p1")  
 
      // output table (prodid pname, price for Product, prodid. depid, quantity for Stock
       while (rs.next()) {
